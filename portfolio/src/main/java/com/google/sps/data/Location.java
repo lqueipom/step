5b5040526c4c7e95c67509d41404ea72bac258bc
Laura <lqueipom@google.com>
2020-07-08 18:00:16 +0000

    Importing Data

---
+++ portfolio/src/main/java/com/google/sps/data/Location.java
@@ -0,0 +1,13 @@
+
+package com.google.sps.data;
+
+/** Represents a location at a specific lat lng point. */
+public class Location {
+  private double lat;
+  private double lng;
+
+  public Location(double lat, double lng) {
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -178,4 +178,24 @@ function visualizeMap() {
   map.setMapTypeId('styles_map');
   homeInfoWindow.open(map, markerHome);
   lakeInfoWindow.open(map, markerLake);
+
+  // favoritePlaces();
+}
+
+function favoritePlaces() {
+  fetch('/interactive').then(response => response.json()).then((json) => {
+    const favoriteMap = new google.maps.Map(
+      document.getElementById('mapTwo'),
+      {
+        center: {
+          lat: 9.815833, 
+          lng: -71.556664
+        }, 
+        zoom: 7,
+    });
+    json.forEach((location) => {
+      new google.maps.Marker(
+        {position: {lat: location.lat, lng: location.lng}, favoriteMap});
+    });
+  });
 }
