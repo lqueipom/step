99f9ab10ead7f9e9db9ce3ce668f67f488085903
Laura <lqueipom@google.com>
2020-07-15 16:35:40 +0000

    Making code more compact

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,10 +24,58 @@ import java.util.Arrays;
 // Meeting request: duration and attendes. 
 // Event: Time range, name of event, attendes of event. 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     
-    // Duration of meeting is more than allowed.
+  public ArrayList<TimeRange> gettingCalendar(
+                                                ArrayList<TimeRange> timeBlocks,
+                                                ArrayList<TimeRange> notAvailableTimes,
+                                                ArrayList<TimeRange> availableTimes,
+                                                long duration
+                                              ) {
+    TimeRange current = timeBlocks.get(0);
+    for (TimeRange time: timeBlocks) {
 
+      if (time.overlaps(current)) {
+        int start = current.start();
+          if (time.end() < current.end()) {
+            current = TimeRange.fromStartEnd(start, current.end(), false);
+          } else {
+          current = TimeRange.fromStartEnd(start, time.end(), false);
+          }
+
+      } else {
+        notAvailableTimes.add(current);
+        current = time;
+        }
+    }
+    // Adding last one, we now have the full calendar of not available time ranges. 
+    notAvailableTimes.add(current);
+    int numberOfTimes = notAvailableTimes.size();
+    TimeRange first = notAvailableTimes.get(0);
+    TimeRange last = notAvailableTimes.get(numberOfTimes-1);
+
+    TimeRange firstHalf = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, first.start(), false);
+    TimeRange secondHalf = TimeRange.fromStartEnd(last.end(), TimeRange.END_OF_DAY, true);
+
+    if (firstHalf.duration() >= duration) {
+      availableTimes.add(firstHalf);
+    }
+    if (secondHalf.duration() >= duration) {
+      availableTimes.add(secondHalf);
+    }
+    
+    if (numberOfTimes > 1) {
+
+      for (int j = 0; j < numberOfTimes - 1; j++) {
+        TimeRange newTime = TimeRange.fromStartEnd(notAvailableTimes.get(j).end(), notAvailableTimes.get(j + 1).start(), false);
+        if (newTime.duration() >= duration) {
+          availableTimes.add(newTime);
+        }
+      }
+    }
+    return availableTimes;                              
+  }
+
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     ArrayList<String> attendees = new ArrayList<>(request.getAttendees());
     int amountAttendees = attendees.size();
     ArrayList<String> optionalAttendees = new ArrayList<>(request.getOptionalAttendees());
@@ -79,106 +127,32 @@ public final class FindMeetingQuery {
     }
     
     if (timeBlocks.isEmpty() == false) {
-      TimeRange current = timeBlocks.get(0);
-      for (TimeRange time: timeBlocks) {
-
-        if (time.overlaps(current)) {
-          int start = current.start();
-          if (time.end() < current.end()) {
-            current = TimeRange.fromStartEnd(start, current.end(), false);
-          } else {
-            current = TimeRange.fromStartEnd(start, time.end(), false);
-          }
-
-        } else {
-          notAvailableTimes.add(current);
-          current = time;
-        }
-      }
-      // Adding last one, we now have the full calendar of not available time ranges. 
-      notAvailableTimes.add(current);
-      int numberOfTimes = notAvailableTimes.size();
-      TimeRange first = notAvailableTimes.get(0);
-      TimeRange last = notAvailableTimes.get(numberOfTimes-1);
-
-      TimeRange firstHalf = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, first.start(), false);
-      TimeRange secondHalf = TimeRange.fromStartEnd(last.end(), TimeRange.END_OF_DAY, true);
-
-      if (firstHalf.duration() >= duration) {
-        availableTimes.add(firstHalf);
-      }
-      if (secondHalf.duration() >= duration) {
-        availableTimes.add(secondHalf);
-      }
-        
-      if (numberOfTimes > 1) {
-
-        for (int j = 0; j < numberOfTimes - 1; j++) {
-          TimeRange newTime = TimeRange.fromStartEnd(notAvailableTimes.get(j).end(), notAvailableTimes.get(j + 1).start(), false);
-          if (newTime.duration() >= duration) {
-            availableTimes.add(newTime);
-          }
-        }
-      }
-      Collections.sort(availableTimes, TimeRange.ORDER_BY_START);
-      ArrayList<TimeRange> calendar = new ArrayList<>(availableTimes);
+      ArrayList<TimeRange> finalCalendar = gettingCalendar(timeBlocks, notAvailableTimes, availableTimes, duration);
+      
+      Collections.sort(finalCalendar, TimeRange.ORDER_BY_START);
+      ArrayList<TimeRange> calendar = new ArrayList<>(finalCalendar);
       for (TimeRange opt: optionalTimeBlocks) {
-        for (TimeRange permanent: availableTimes) {
+        for (TimeRange permanent: finalCalendar) {
           if (permanent.overlaps(opt)) {
             calendar.remove(permanent);
           }
         }
       }
       if (calendar.isEmpty()) {
-        return availableTimes;
+        return finalCalendar;
       } else {
         return calendar;
       }
 
     } else {
-
-      TimeRange current = optionalTimeBlocks.get(0);
-      for (TimeRange time:optionalTimeBlocks) {
-
-        if (time.overlaps(current)) {
-          int start = current.start();
-    
-          if (time.end() < current.end()) {
-            current = TimeRange.fromStartEnd(start, current.end(), false);
-          } else {
-            current = TimeRange.fromStartEnd(start, time.end(), false);
-          }
-
-        } else {
-          notOptionalTimes.add(current);
-          current = time;
-        }
-      }
-      // Adding last one, we now have the full calendar of not available time ranges. 
-      notOptionalTimes.add(current);
-      int numberOfTimes = notOptionalTimes.size();
-      TimeRange first = notOptionalTimes.get(0);
-      TimeRange last = notOptionalTimes.get(numberOfTimes-1);
-      TimeRange firstHalf = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, first.start(), false);
-      TimeRange secondHalf = TimeRange.fromStartEnd(last.end(), TimeRange.END_OF_DAY, true);
-
-      if (firstHalf.duration() >= duration) {
-        optionalCalendar.add(firstHalf);
-      }
-      if (secondHalf.duration() >= duration) {
-        optionalCalendar.add(secondHalf);
-      }
-      if (numberOfTimes > 1) {
-
-        for (int j = 0; j < numberOfTimes - 1; j++) {
-          TimeRange newTime = TimeRange.fromStartEnd(notOptionalTimes.get(j).end(), notOptionalTimes.get(j + 1).start(), false);
-          if (newTime.duration() >= duration) {
-            optionalCalendar.add(newTime);
-          }
-        }
-      }     
-      Collections.sort(optionalCalendar, TimeRange.ORDER_BY_START);
-      return optionalCalendar;
+      ArrayList<TimeRange> finalOptionalCalendar = gettingCalendar(
+                                                                    optionalTimeBlocks, 
+                                                                    notOptionalTimes, 
+                                                                    optionalCalendar, 
+                                                                    duration
+                                                                  );
+      Collections.sort(finalOptionalCalendar, TimeRange.ORDER_BY_START);
+      return finalOptionalCalendar;
     }
   }
 }
